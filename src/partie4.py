2c26313102eafa98c01af8d00f2ee15487a06422
Simon DUCAMP <simon.ducamp@enseirb-matmeca.fr>
2023-03-08 16:12:15 +0100

    Push intermédiaire

---
+++ src/partie4.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    img_full = plt.imread("../files/p3_takeoff_base.png")
+    n, m, colors = img_full.shape
+    
