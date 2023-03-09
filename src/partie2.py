597902f8ae70edd6ffd2372a15a34ed1a7b2f08f
Simon DUCAMP <simon.ducamp@enseirb-matmeca.fr>
2023-03-09 12:12:37 +0100

    Part2 includes HH matrix

--- src/partie2.py
+++ src/partie2.py
@@ -1,18 +1,32 @@
 import numpy as np
 
-def to_bidiag(Qleft, Qright, BD):
-    for i in range(np.shape(BD)[0]):
-        Q1 = #create HH where U = B[i:n,i] and V = ...
+import partie1
+
+""" Creates a vector with a single non-zero element (index i)"""
+def vect_ei(n,i):
+    vect = np.zeros(n)
+    vect[i] = 1
+    return vect
+
+def to_bidiag(BD):
+    n = np.shape(BD)[0]
+    m = np.shape(BD)[1]
+    Qleft = np.identity(n)
+    Qright = np.identity(n)
+    for i in range(n):
+        Q1 = partie1.householder_mat(BD[i:n,i], vect_ei(n, i))
         Qleft = Qleft@Q1
         BD = Q1@BD
-        if i != (np.shape(A)[1] - 2):
-            Q2 = #create HH where U = B[i,(i+i):m] and V = ...
+        if i != (m - 2):
+            Q2 = partie1.householder_mat(BD[i,(i+1):m], vect_ei(m-1, i))
             Qright = Q2@Qright
             BD = BD@Q2
     return (Qleft, BD, Qright)
 
 if __name__ == "__main__":
-    Q2 = np.identity(4)
-    Q1 = np.identity(4)
-    A = np.array([[1,2,3,4], [2,3,4,5], [3,4,5,6], [4,5,6,7]])
-    print(to_bidiag(Q1,Q2,A))
+    A = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]])
+    print(A[0,(0+1):np.shape(A)[1]])
+    print(to_bidiag(A))
+
+    # A = np.array([[1,2,3,4], [2,3,4,5], [3,4,5,6]])
+    # print(to_bidiag(Q1,Q2,A))
