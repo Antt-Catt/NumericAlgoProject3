8b635a06cb2a068372ffb67b936f0c2a494243e9
Simon DUCAMP <simon.ducamp@enseirb-matmeca.fr>
2023-03-10 14:04:19 +0100

    Tests part2

--- src/partie2.py
+++ src/partie2.py
@@ -12,15 +12,22 @@ def to_bidiag(BD):
     n = np.shape(BD)[0]
     m = np.shape(BD)[1]
     Qleft = np.identity(n)
-    Qright = np.identity(n)
+    Qright = np.identity(m)
+    print("Init Qright = ", Qright)
     for i in range(n):
         Q1 = partie1.householder_mat(BD[i:n,i], vect_ei(n, i))
+        print("Q1 = ", Q1)
         Qleft = Qleft@Q1
+        print("Qleft = ", Qleft)
         BD = Q1@BD
         if i != (m - 2):
             Q2 = partie1.householder_mat(BD[i,(i+1):m], vect_ei(m-1, i))
+            print("Q1 = ", Q1)
+            print("Q2 = ", Q2)
             Qright = Q2@Qright
+            print("Qright = ", Qright)
             BD = BD@Q2
+        print("BD = ", BD)
     return (Qleft, BD, Qright)
 
 if __name__ == "__main__":
