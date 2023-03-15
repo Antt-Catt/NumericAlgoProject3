4f2f07c7479285ab6100fcc8341dd9147c40a157
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2023-03-15 17:35:31 +0100

    part3 'mise en forme'

--- src/partie3.py
+++ src/partie3.py
@@ -11,11 +11,28 @@ def transfo_qr(U, V, S):
         V = Q1.T@V
     return (U, S, V)
 
+def change_U_S(U, S):
+    n, m = S.shape
+    for i in range(m):
+        if (res[1])[i][i] < 0:
+            for j in range(n):
+                 (res[0])[j][i] = -(res[0])[j][i]
+            (res[1])[i][i] = -(res[1])[i][i]
+    return (res[0], res[1])
+        
+
 if __name__ == "__main__":
-    np.set_printoptions(precision=4, suppress=True)
-    U = np.identity(4)
-    V = np.identity(4)
-    S = np.array([[1,2,0,0],[0,5,8,0],[0,0,3,2],[0,0,0,2]])
-    print(transfo_qr(U,V,S)[0])
-    print(transfo_qr(U,V,S)[1])
-    print(transfo_qr(U,V,S)[2])
+    np.set_printoptions(precision=10, suppress=True)
+    U = np.eye(4,4)
+    V = np.eye(4,4)
+    S = np.array([[1,2,0,0],[0,3,4,0],[0,0,5,6],[0,0,0,7]])
+
+    res = transfo_qr(U,V,S)
+
+    new = change_U_S(res[0], res[1]) # U et S modifiées pour que elts de S soient > 0 et décroissants
+    print(res[0], "U")
+    print(res[1], "S")
+    print(res[2], "V")
+
+    print("UxSxV =", new[0]@new[1]@res[2])
+    print("BD =", S)
