bc341ab88866949229fe5d54d2d614462bd5c08b
Simon DUCAMP <simon.ducamp@enseirb-matmeca.fr>
2023-03-20 11:53:11 +0100

    Oubli de fichiers sources

--- src/partie2.py
+++ src/partie2.py
@@ -8,6 +8,8 @@ def vect_ei(n,i):
     vect[i] = 1
     return vect
 
+
+
 def padWithId(Q, n):
     k = Q.shape[0]
     pad_width = ((n-k, 0), (n-k, 0))  # la taille du padding
@@ -32,13 +34,42 @@ def to_bidiag(BD):
             Q2 = padWithId(Q2,m)
             Qright = Q2@Qright
             BD = BD@Q2
+        print(Qleft @ BD @ Qright)
     return (Qleft, BD, Qright)
 
 
+
+
+def to_bidiag_opti(BD):
+    n, m = BD.shape
+    Qleft = np.identity(n)
+    Qright = np.identity(m)
+    for i in range(n):
+        Q1 = partie1.produit_mat_opti(BD[i:n,i], vect_ei(n-i, 0) * np.linalg.norm(BD[i:n,i]), Qleft[i:,i:])
+        Qleft[i:,i:] = Q1
+        BD = Qleft@BD
+        if i==m-1 :
+            break
+        if i != (m - 2) :
+            Q2 = partie1.produit_mat_opti(BD[i,(i+1):m], vect_ei(m-i-1, 0)* np.linalg.norm(BD[i,(i+1):m]), Qright[i+1:,i+1:])
+            Qright[i+1:,i+1:] = Q2
+            BD = BD@Qright
+        print(Qleft @ BD @ Qright)
+    return (Qleft, BD, Qright)
+
 if __name__ == "__main__":
     A = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]])
-    print(to_bidiag(A))
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.1f}".format(x)})
+    print(to_bidiag(A)[1])
+
+    print("\n\n\n")
 
     A = np.array([[1,2,3], [3,4,5], [5,6,7], [7,8,9]])
     np.set_printoptions(formatter={'float': lambda x: "{0:0.1f}".format(x)})
-    print(to_bidiag(A))
+    print(to_bidiag(A)[1])
+
+    print("\n\n\n")
+
+    A = np.array([[1,2,3], [3,4,5], [5,6,7], [7,8,9]])
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.1f}".format(x)})
+    print(to_bidiag_opti(A)[1])
